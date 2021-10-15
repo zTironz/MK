6fcc2db6569eecd72cdfe92c1679cc535c1387f4
Tiron <ole_g92@bk.ru>
2021-10-15 15:01:04 +0300

    changes

--- main.js
+++ main.js
@@ -56,38 +56,3 @@ function createPlayer(player, myHero,) {
 
 createPlayer('player1', Scorpion);
 createPlayer('player2', Subzero);
-
-
-
-
-
-
-
-// function createPlayer(player, name, hp) {
-//     const player1 = document.createElement('div');
-//     player1.classList.add(player);
-//     const progressbar = document.createElement('div');
-//     progressbar.classList.add('progressbar');
-//     const character = document.createElement('div');
-//     character.classList.add('character');
-//     player1.appendChild(progressbar)
-//     player1.appendChild(character)
-//     const namee = document.createElement('div');
-//     namee.classList.add('name');
-//     namee.textContent = name;
-//     const life = document.createElement('div');
-//     life.classList.add('life');
-//     life.style.width = `${hp}%`;
-//     progressbar.appendChild(namee);
-//     progressbar.appendChild(life);
-//     const img = document.createElement('img');
-//     img.classList.add('img');
-//     character.appendChild(img);
-//     img.src = 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif';
-//     const arenas = document.querySelector('.arenas');
-//     console.log(arenas)
-//     arenas.appendChild(player1)
-// }
-
-// createPlayer('player1', 'SCORPION', 50);
-// createPlayer('player2', 'SUB-ZERO', 80);
