d8d6280a2ffa74fee678f3b7f84016c5dc8de993
Tiron <ole_g92@bk.ru>
2021-10-20 18:20:33 +0300

    homework5

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 console.log('Mortal Kombat')
 
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+}
+const ATTACK = ['head', 'body', 'foot'];
+
 const arenas = document.querySelector('.arenas');
 const randomButton = document.querySelector('.button');
+const formFight= document.querySelector('.control');
 
 const player1= {
     player: 1,
@@ -12,9 +20,9 @@ const player1= {
     attack: function() {
         this.name + 'Fight...'
     },
-    changeHp: changeHp,
-    renderHP: renderHP,
-    elHP: elHP
+    changeHp,
+    renderHP,
+    elHP
 }
 
 const player2= {
@@ -26,9 +34,9 @@ const player2= {
     attack: function() {
         this.name + 'Fight...'
     },
-    changeHp: changeHp,
-    renderHP: renderHP,
-    elHP: elHP
+    changeHp,
+    renderHP,
+    elHP
 }
 
 function createElement(tag, className) {
@@ -72,15 +80,6 @@ function changeHp(num) {
     if (this.hp <= 0) {
         this.hp = 0;
     }
-    // const plyaerLife = document.querySelector('.player' + player.player + ' .life');
-    // player.hp -= randomNumber(20);
-    // console.log(player.hp)
-   
-
-    // if(player.hp <= 0) {
-    //     player.hp = 0;
-    // }
-    // plyaerLife.style.width = player.hp + '%'; 
 }
 
 function elHP() {
@@ -93,7 +92,6 @@ function renderHP() {
     let hpRender = this.elHP()
     hpRender.style.width = this.hp + '%'
 }
-
 
 
 function winHero(name) {
@@ -122,31 +120,96 @@ function createReloadButton() {
 }
 
 
-randomButton.addEventListener('click', function() {
-    // changeHp(player1)
-    // changeHp(player2)
-    console.log(player1.hp)
-    player1.changeHp(randomNumber(20))
-    console.log(player1.hp)
-    player1.renderHP()
-    player2.changeHp(randomNumber(20))
-    player2.renderHP()
+// randomButton.addEventListener('click', function() {
+//     // changeHp(player1)
+//     // changeHp(player2)
+//     console.log(player1.hp)
+//     player1.changeHp(randomNumber(20))
+//     console.log(player1.hp)
+//     player1.renderHP()
+//     player2.changeHp(randomNumber(20))
+//     player2.renderHP()
+
+//     if( player1.hp === 0 || player2.hp === 0 ) {
+//         randomButton.disabled = true;
+//         createReloadButton()
+//     }
+
+//     if(player1.hp === 0 && player1.hp < player2.hp) {
+//         arenas.appendChild(winHero(player2.name));
+        
+//     } else if(player2.hp === 0 && player2.hp < player1.hp) {
+//         arenas.appendChild(winHero(player1.name));
+       
+//     } else if ( player1.hp === 0 && player2.hp ===0 ){
+//         arenas.appendChild(winHero());
+        
+//     }
+// })
+
+arenas.appendChild(createPlayer(player1));
+arenas.appendChild(createPlayer(player2));
+
+function enemyAttack() {
+    const hit = ATTACK[randomNumber(3) - 1];
+    const defence = ATTACK[randomNumber(3) - 1];
+    
+    return {
+        value: randomNumber(HIT[hit]),
+        hit,
+        defence
+    }
+
+}
+
+formFight.addEventListener('submit', function(e) {
+    e.preventDefault();
+    console.dir(formFight)
+    const enemy = enemyAttack()
+    const attack = {};
+
+    for(let item of formFight) {
+        if(item.checked && item.name === "hit") {
+            attack.value = randomNumber(HIT[item.value]);
+            attack.hit = item.value;
+        }
+        if(item.checked && item.name === "defence") {
+            attack.defence = item.value;
+        }
+        item.checked = false;
+    }
+    console.log(attack, 'atack')
+    console.log(enemy, 'enemy')
+
+
+    if(attack.hit != enemy.defence) {
+        player2.changeHp(attack.value);
+        player2.renderHP();
+    }
+
+    if(enemy.hit != attack.defence) {
+        player1.changeHp(enemy.value);
+        player1.renderHP();
+    }
+
+    // player1.changeHp(enemy.value)
+    // player1.renderHP()
+    // player2.changeHp(attack.value)
+    // player2.renderHP()
 
     if( player1.hp === 0 || player2.hp === 0 ) {
         randomButton.disabled = true;
+        createReloadButton()
     }
 
     if(player1.hp === 0 && player1.hp < player2.hp) {
         arenas.appendChild(winHero(player2.name));
-        createReloadButton()
+        
     } else if(player2.hp === 0 && player2.hp < player1.hp) {
         arenas.appendChild(winHero(player1.name));
-        createReloadButton()
+       
     } else if ( player1.hp === 0 && player2.hp ===0 ){
         arenas.appendChild(winHero());
-        createReloadButton()
+        
     }
 })
-
-arenas.appendChild(createPlayer(player1));
-arenas.appendChild(createPlayer(player2));
